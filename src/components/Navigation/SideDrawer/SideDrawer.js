15151d50f0ad50585ee0547c5acc296eee3766d9
Biplov Bhandari <bionicbiplov45@gmail.com>
2020-03-27 23:19:43 +0545

    initial working version

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
+
+const sideDrawer = (props) => {
+    const attachedClasses = (props.open) ? [classes.SideDrawer, classes.Open] : [classes.SideDrawer, classes.Close];
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+};
+
+export default sideDrawer;
